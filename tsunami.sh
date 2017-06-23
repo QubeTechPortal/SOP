70dd5ddc5494bb6e5f70e9f1ee4ae605ca333b89
Arvindthiru <tionarvind@ymail.com>
2017-06-23 21:42:54 +0530

    Create tsunami.sh

---
+++ tsunami.sh
@@ -0,0 +1,5 @@
+apt get-install cvs git gcc automake autoconf
+cvs -z3 d:pserver:anonymous@tsunamiudp.cvs.sourceforge.net:/cvsroot/tsunami-udp co -P tsunami-udp
+cd tsunami-udp
+./recompile.sh
+make install
